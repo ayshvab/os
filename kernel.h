08e492602f89f3b7916f7b379190ec67479df90e
Anton <ays@f-m.fm>
2025-05-25 21:13:32 +0300

    vm: page table

--- kernel.h
+++ kernel.h
@@ -68,6 +68,13 @@ struct process {
 	int pid;
 	int state;
 	vaddr_t sp;
+	uint32_t* page_table;
 	uint8_t stack[8192]; // kernel (not user!) stack. Each process has own independent kernel stack
 };
 
+#define SATP_SV32 (1u << 31)
+#define PAGE_V    (1 << 0)   // "Valid" bit (entry is enabled)
+#define PAGE_R    (1 << 1)   // Readable
+#define PAGE_W    (1 << 2)   // Writable
+#define PAGE_X    (1 << 3)   // Executable
+#define PAGE_U    (1 << 4)   // User (accessible in user mode)
