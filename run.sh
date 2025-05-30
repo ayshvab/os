5d39af4fdc7dbc470e65596693c820e856d160c0
Anton <ays@f-m.fm>
2025-05-30 18:00:24 +0300

    update notes and run.sh

--- run.sh
+++ run.sh
@@ -9,6 +9,7 @@ CFLAGS="-std=c11 -O2 -g3 -Wall -Wextra --target=riscv32-unknown-elf -fno-stack-p
 $CC $CFLAGS -Wl,-Tkernel.ld -Wl,-Map=kernel.map -o kernel.elf \
     kernel.c common.c
 
-# Start QEMU
-$QEMU -machine virt -bios default -nographic -serial mon:stdio --no-reboot \
+$QEMU -machine virt -bios opensbi-riscv32-generic-fw_dynamic.bin -nographic -serial mon:stdio --no-reboot \
+    -d unimp,guest_errors,int,cpu_reset -D qemu.log \
     -kernel kernel.elf
+
